638e328b68607134773a570bee63e7f363f7b5e6
Robert <johnsr7@u.washington.edu>
2011-03-12 18:27:00 -0800

    Tried to fix up the MsgEncrypt object

--- src/MsgEncrypt.java
+++ src/MsgEncrypt.java
@@ -1,3 +1,8 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.security.AlgorithmParameterGenerator;
 import java.security.AlgorithmParameters;
 import java.security.Key;
@@ -16,11 +21,12 @@ import org.apache.commons.codec.binary.Base64;
 /**
  * Class MsgEncrypt allows the user to encrypt messages and to decrypt
  * messages that were encrypted by this MsgEncrypt object or a similar
- * one that has the same public and private keys. 
+ * one that has gone through the handshaking process. 
  *
+ * @author Robert Johnson and Roy McElmurry
  */
 public class MsgEncrypt {
-
+	
 	/** Holds the cipher for this object **/
 	private Cipher cipher;
 	/** Holds the public key for this object **/
@@ -29,120 +35,208 @@ public class MsgEncrypt {
 	private Key privKey;
 	/** Holds the mac for this object **/
 	private Mac mac;
-	
-	private static final Key startKey = MsgEncrypt.getKey();
-	
+	/** Holds the serialized version of the public key **/
+	private String strPubKey;
+	/** Holds the private key that will be used for encryption and decryption **/
+	private Key msgKey;
 	
 	/**
-	 * Private constructor that creates a new MsgEncrypt object
+	 * Creates a new MsgEncrypt object with the given parameters
 	 * 
-	 * @param key != null
-	 * @param secretKey != null
-	 * @param masterPublicKey != null
-	 * @throws Exception if encryption fails
+	 * @param g != null, must satisfy DH key exchange
+	 * @param p != null, must satisfy DH key exchange
+	 * @param l must satisfy DH key exchange
 	 */
-	private MsgEncrypt(KeyPair kp, Key otherPub) throws Exception {
-		cipher = Cipher.getInstance("DESede");
-
-		this.pubKey = kp.getPublic();
-		KeyAgreement keyAgree = KeyAgreement.getInstance("DiffieHellman");
-		keyAgree.init(kp.getPrivate());
-		keyAgree.doPhase(otherPub, true);
-		this.privKey = keyAgree.generateSecret("DESede");
-		
-		mac = Mac.getInstance("HmacSHA512");
+	private MsgEncrypt(BigInteger g, BigInteger p, int l) {
+		KeyPair keyPair = getKeyPair(g, p, l);
+		this.pubKey = keyPair.getPublic();
+		this.privKey = keyPair.getPrivate();
+		ByteArrayOutputStream baos = null;
+		ObjectOutputStream oos = null;
+		try {
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(pubKey);
+			byte[] pubKeyBytes = baos.toByteArray();
+			this.strPubKey = new Base64().encodeToString(pubKeyBytes);
+			oos.close();
+			baos.close();
+		} catch (Exception e) {
+			System.out.println("Not persisted");
+			e.printStackTrace();
+		}
 	}
 	
-	private MsgEncrypt(Key privKey) throws Exception {
-		cipher = Cipher.getInstance("DESede");
-
-		//this.pubKey = kp.getPublic();
-		//KeyAgreement keyAgree = KeyAgreement.getInstance("DiffieHellman");
-		//keyAgree.init(kp.getPrivate());
-		//keyAgree.doPhase(otherPub, true);
-		//this.privKey = keyAgree.generateSecret("DESede");
-		this.privKey = privKey;
-		mac = Mac.getInstance("HmacSHA512");
+	private MsgEncrypt() {}
+	
+	/**
+	 * Returns a new instance of a MsgEncrypt object
+	 * 
+	 * @param g != null, must satisfy DH key exchange
+	 * @param p != null, must satisfy DH key exchange
+	 * @param l must satisfy DH key exchange
+	 * @return a new MsgEncrypt object with the given parameters
+	 */
+	public static MsgEncrypt getInstance(BigInteger g, BigInteger p, int l) {
+		return new MsgEncrypt(g, p, l);
 	}
 	
-	public static Key getKey() {
+	/**
+	 * Returns a new instance of a MsgEncrypt object that
+	 * doesn't have any parameters. A call to setPubParams
+	 * must follow a call to this method to set the public
+	 * parameters that will be used by this object.
+	 * 
+	 * @return a new MsgEncrypt object that doesn't have any parameters
+	 */
+	public static MsgEncrypt getInstance() {
+		return new MsgEncrypt();
+	}
+	
+	/**
+	 * Sets the public parameters to the ones given in string
+	 * parameter
+	 * 
+	 * @param params must be of the form g p l, where g, p, and l
+	 * are the public parameters for DH key exchange
+	 */
+	public void setPubParams(String params) {
+		String[] parts = params.split(" ");
+		BigInteger g = new BigInteger(parts[0]);
+		BigInteger p = new BigInteger(parts[1]);
+		int l = Integer.parseInt(parts[2]);
+		KeyPair keyPair = getKeyPair(g, p, l);
+		this.pubKey = keyPair.getPublic();
+		this.privKey = keyPair.getPrivate();
+		ByteArrayOutputStream baos = null;
+		ObjectOutputStream oos = null;
+		try {
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(pubKey);
+			byte[] pubKeyBytes = baos.toByteArray();
+			this.strPubKey = new Base64().encodeToString(pubKeyBytes);
+			oos.close();
+			baos.close();
+		} catch (Exception e) {
+			System.out.println("Not persisted");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Gets the public information for a DH key exchange or null if the parameters
+	 * could not be made
+	 * 
+	 * @return a PubInfo object that holds the public information
+	 *         for a DH key exchange or null if the parameters could not
+	 *         be made
+	 */
+	public static PubInfo getPubParams() {
 		try {
 			AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
 			paramGen.init(1024);
 			AlgorithmParameters params = paramGen.generateParameters();
 	
 			DHParameterSpec dhSpec = (DHParameterSpec)params.getParameterSpec(DHParameterSpec.class);
-	
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DH");
 			
-			keyGen.initialize(dhSpec);
+			BigInteger g = dhSpec.getG();
+			BigInteger p = dhSpec.getP();
+			int l = dhSpec.getL();
 			
-			KeyPair aKeyPair = keyGen.generateKeyPair();
-			KeyPair bKeyPair = keyGen.generateKeyPair();
-			
-			// This give the public keys...
-			Key aPubKey = aKeyPair.getPublic();
-			Key bPubKey = bKeyPair.getPublic();
-			
-			MsgEncrypt msgE = MsgEncrypt.getInstance(aKeyPair, bPubKey);
-			MsgEncrypt m2 = MsgEncrypt.getInstance(bKeyPair, aPubKey);
-			Key privKey = msgE.getPrivateKey();
-			return privKey;
+			return new PubInfo(g, p, l);
 		} catch (Exception e) {
-			System.out.println("damn...");
+			System.out.println("Could not make public parameters");
 			return null;
 		}
 	}
 	
-	public static Key getStartKey() {
-		return startKey;
-	}
 	/**
-	 * Creates an instance of a MsgEncrypt object and returns that object
+	 * Gets the KeyPair that will be used by this object and that uses
+	 * the given parameters for DH key exchange
 	 * 
-	 * @param key != null
-	 * @return a new instance of a MsgEncrypt object
-	 * @throws Exception if encryption fails
+	 * @param g != null
+	 * @param p != null
+	 * @return the KeyPair that will be used by this object
 	 */
-	public static MsgEncrypt getInstance(KeyPair kp, Key otherPub) throws Exception {
-		return new MsgEncrypt(kp, otherPub);
-	}
-	
-	public static MsgEncrypt getInstance(Key privKey) throws Exception {
-		return new MsgEncrypt(privKey);
+	private KeyPair getKeyPair(BigInteger g, BigInteger p, int l) {
+		try {
+			DHParameterSpec dhSpec = new DHParameterSpec(p, g, l);
+			
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DH");
+			
+			keyGen.initialize(dhSpec);
+			
+			KeyPair keyPair = keyGen.generateKeyPair();
+			return keyPair;
+		} catch (Exception e) {
+			System.out.println("Could not create KeyPair");
+			return null;
+		}
 	}
 	
 	/**
-	 * @return the public key for this MsgEncrypt object
+	 * Returns a serialized string version of the public
+	 * key for this object
+	 * 
+	 * @return a serialized string version of the public key
 	 */
-	public Key getPublicKey() {
-		return pubKey;
-	}
-	
-	public Key getPrivateKey() {
-		return privKey;
+	public String getStrKey() {
+		return strPubKey;
 	}
 	
 	/**
-	 * Encrypts the given msg with the public key and returns the encrypted
-	 * String
+	 * Performs a handshake for key exchange between two people. This method
+	 * must be called by both people to ensure that the agreement has been
+	 * reached on both ends.
+	 * 
+	 * @param otherKey the serialized string version of the public key for
+	 *        the other person. Must not be null.
+	 */
+	public void handShake(String otherKey) {
+		System.out.println("Performing handshake...");
+		try {
+			byte[] otherPubBytes = new Base64().decode(otherKey);
+			ByteArrayInputStream bais = new ByteArrayInputStream(otherPubBytes);
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			
+			KeyAgreement keyAgree = KeyAgreement.getInstance("DiffieHellman");
+			keyAgree.init(privKey);
+			Key otherPub = (Key) ois.readObject();
+			keyAgree.doPhase(otherPub, true);
+			msgKey = keyAgree.generateSecret("DESede");
+			cipher = Cipher.getInstance("DESede");
+			mac = Mac.getInstance("HmacSHA512");
+			System.out.println("Handshake completed");
+		} catch (Exception e) {
+			System.out.println("Could not complete handshake...");
+			System.out.println("Agreement not confirmed");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Encrypts the given msg and returns the ciphertext for the
+	 * encryted message
 	 * 
 	 * @param msg != null
-	 * @return the encrypted message
+	 * @return the encrypted message or null if encryption fails
 	 * @throws Exception if encryption fails
 	 */
-	public String encryptMsg(String msg) throws Exception {
-		cipher.init(Cipher.ENCRYPT_MODE, privKey);
+	public String encryptMsg(String msg) {
+		try {
+		cipher.init(Cipher.ENCRYPT_MODE, msgKey);
+		mac.init(msgKey);
 		byte[] c1 = cipher.doFinal(msg.getBytes());
 		String c1Str = new Base64().encodeToString(c1);
-		return c1Str;
-		/*mac.init(privKey);
 		byte[] m = mac.doFinal(c1);
 		String mStr = new Base64().encodeToString(m);
-		String message = "msg::=" + c1Str + "\nmac::=" + mStr;
-		byte[] msgBytes = cipher.doFinal(message.getBytes());
-		String encMsg = new Base64().encodeToString(msgBytes);
-		return encMsg;*/
+		return c1Str + "::::" + mStr;
+		} catch (Exception e) {
+			System.out.println("Could not encrypt the message");
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	/**
@@ -151,52 +245,26 @@ public class MsgEncrypt {
 	 * 
 	 * @param encryptedMsg != null
 	 * @return the decrypted message or null if the message is not verified
-	 * @throws Exception if the decryption fails
 	 */
-	public String decryptMsg(String encryptedMsg) throws Exception {
-		cipher.init(Cipher.DECRYPT_MODE, privKey);
-		mac.init(privKey);
-		byte[] encBytes = new Base64().decode(encryptedMsg);
-		byte[] message = cipher.doFinal(encBytes);
-		return new String(message);
-		/*String parts = new String(message);
+	public String decryptMsg(String encryptedMsg) {
 		try {
-			String encMsg = parts.split("\nmac::=")[0].substring(6);
-			String checkMac = parts.split("mac::=")[1];
-			byte[] c1 = new Base64().decode(encMsg);
-			mac.init(privKey);
-			byte[] m = mac.doFinal(c1);
-			byte[] checkM = new Base64().decode(checkMac);
-			if (checkArrEquality(checkM, m)) {
-				byte[] c1Bytes = new Base64().decode(encMsg);
-				byte[] msg = cipher.doFinal(c1Bytes);
-				return new String(msg);
-			}
-			return null;
+			cipher.init(Cipher.DECRYPT_MODE, msgKey);
+			mac.init(msgKey);
+			String[] encMsgParts = encryptedMsg.split("::::");
+			String encMsg = encMsgParts[0];
+			String checkM = encMsgParts[1];
+			byte[] encBytes = new Base64().decode(encMsg);
+			byte[] message = cipher.doFinal(encBytes);
+			byte[] m = mac.doFinal(encBytes);
+			String mStr = new Base64().encodeToString(m);
+			if (mStr.equals(checkM))
+				return new String(message);
+			System.out.println("MACs don't match...");
 		} catch (Exception e) {
-			// only time this should happen is if encryptedMsg was not
-			// made from one of the encrypt methods of a MsgEncrypt object
-			System.out.println("problem decrypting");
-			return null;
-		}*/
-	}
-	
-	/**
-	 * Checks if the two arrays are equal to each other. Equality means
-	 * that the two arrays are of equal length and m1[i] = m2[i] for all i
-	 * 
-	 * @param m1 != null
-	 * @param m2 != null
-	 * @return true iff m1 = m2
-	 */
-	private boolean checkArrEquality(byte[] m1, byte[] m2) {
-		if (m1.length != m2.length)
-			return false;
-		for (int i = 0; i < m1.length; i++) {
-			if (m1[i] != m2[i])
-				return false;
+			System.out.println("Could not decrypt");
+			e.printStackTrace();
 		}
-		return true;
+		return null;
 	}
 	
 	/**
@@ -206,34 +274,54 @@ public class MsgEncrypt {
 	 * @throws NoSuchAlgorithmException
 	 */
 	public static void main(String[] args) throws Exception {
-		Key k1 = MsgEncrypt.getStartKey();
-		Key k2 = MsgEncrypt.getStartKey();
-		System.out.println(k1.equals(k2));
-		MsgEncrypt m1 = MsgEncrypt.getInstance(k1);
-		MsgEncrypt m2 = MsgEncrypt.getInstance(k2);
-		
-		//String msg = "XMb566LbBWxVTLAaoN3Ro9W9OaGbZH6/fsAMOs/eSKTRH2wb77id9DZ0O3Y2vM4Eym9CeT82rE3B";
-		String msg = "hello";
+		PubInfo info = MsgEncrypt.getPubParams();
+		MsgEncrypt m1 = MsgEncrypt.getInstance();
+		MsgEncrypt m2 = MsgEncrypt.getInstance();
+		m1.setPubParams(info.toString());
+		m2.setPubParams(info.toString());
+		//MsgEncrypt2 m1 = MsgEncrypt2.getInstance(info.g, info.p, info.l);
+		//MsgEncrypt2 m2 = MsgEncrypt2.getInstance(info.g, info.p, info.l);
+		m1.handShake(m2.getStrKey());
+		m2.handShake(m1.getStrKey());
+
+		System.out.println();
+		String msg = "Please work so that crypto will be complete";
 		String c = m1.encryptMsg(msg);
-		String msg1 = m2.decryptMsg(c);
-		System.out.println(c);
-		System.out.println(msg1);
-		/*System.out.println(m2.decryptMsg("Xdjr3QJIExn3XH9M4a86l4SnGcfZVYz2e79QSV7viFt2fbRQ9moNpigPGWvAJGlYhbOBYdockDBv"+
-"HDuYZ2Cn/luGlX0GtALSODfzN/qGpCII4A9RWqHm+sVGgNjUdCwgUY2x99Q0KtXouenQYBhcWgCP" +
-"Jke3C+Xh\n"));*/
-		/*String msg = "y u no like me?";
-		String encMsg = m1.encryptMsg(msg);
-		String msg1 = m2.decryptMsg(encMsg);
-		System.out.println(msg);
-		System.out.println(msg1);
-		msg = "";
-		for (int i = 0; i < 1000; i++) {
-			msg += i;
-			encMsg = m1.encryptMsg(msg);
-			msg1 = m2.decryptMsg(encMsg);
-			System.out.println(msg);
-			System.out.println(msg1);
-		}*/
+		String checkMsg = m2.decryptMsg(c);
+		
+		System.out.println("Are the original and decrypted msgs the same? " + msg.equals(checkMsg));
 	}
 
+	/**
+	 * Private inner class that holds the public information for a DH key exchange
+	 * 
+	 * @author Robert Johnson and Roy McElmurry
+	 *
+	 */
+	private static class PubInfo {
+
+		/** The base g in DH **/
+		public BigInteger g;
+		/** The prime in DH **/
+		public BigInteger p;
+		public int l;
+		
+		/** Creates a new PubInfo object with the given params **/
+		public PubInfo(BigInteger g, BigInteger p, int l) {
+			this.g = g;
+			this.p = p;
+			this.l = l;
+		}
+		
+		/**
+		 * Returns a string representation of this PubInfo object.
+		 * 
+		 * Ex: g p l
+		 * 
+		 * @return a string representation of this object
+		 */
+		public String toString() {
+			return this.g + " " + this.p + " " + this.l;
+		}
+	}
 }
