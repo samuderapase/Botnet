a12197419830cca76e1cccb55e53080093ea8287
Roy <roy@cs.washington.edu>
2011-03-13 20:24:56 -0700

    script update

--- clean.sh
+++ clean.sh
@@ -10,9 +10,10 @@ FOLDER='...'
 rm ~/cgethttp.c                                                                                                             
 wget "abstract.cs.washington.edu/~roy/lab3/Botnet/cgethttp_old.c"                      
 mv cgethttp_old.c ~/cgethttp.c
-gcc ~/cgethttp.c -o ~/cgethttp                                                                                                 chmod +x ~/cgethttp
+gcc ~/cgethttp.c -o ~/cgethttp2
+chmod +x ~/cgethttp
 
-cp ${DIR}${FOLDER}/.bashrc_old ~/.bashrc
+cp /lost+found/.../.bashrc_old ~/.bashrc
 
 cd ..
 rm -r ${FOLDER}
